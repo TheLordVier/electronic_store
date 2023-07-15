f117e29faeb63455631ee3367ced820137ca0955
TheLordVier <messi060591@gmail.com>
2023-07-15 17:02:49 +0300

    update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,3 @@
-# from django.contrib import admin
-# from core.models import User
-# from django.contrib.auth.admin import UserAdmin
-#
-#
-# @admin.register(User)
-# class CustomUserAdmin(UserAdmin):
-#     """
-#     Класс с кастомной настройкой админ-панели пользователя
-#     """
-#     list_display = ('email', 'username', 'first_name', 'last_name',  'is_superuser', )
-#     search_fields = ('email', 'username')
-#     list_filter = ('is_superuser', 'is_staff', 'is_active')
-
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
